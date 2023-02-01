75b64144a693725f540fb029418c257101dded1b
DanielRStevens <h17daste@du.se>
2023-02-01 13:32:30 +0100

    Co-authored-by: VivianLexy <VivianLexy@users.noreply.github.com> Co-authored-by: Makda777 <Makda777@users.noreply.github.com> Co-authored-by: CasperFa <CasperFa@users.noreply.github.com> Co-authored-by: alexanderstrom1 <alexanderstrom1@users.noreply.github.com>

--- README.md
+++ README.md
@@ -9,3 +9,8 @@
 
 # Introduction
 This is a website for a vintage book store.
+Step1
+We started with Installing the necessary tools
+ 1. Created accounts on Git 
+ 2. Create Git Respository
+ 3. Create React App 
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div class="App">
+      <header class="App-header">
+
       </header>
+      <main class="App-main">
+        <div class="App-categories">text</div>
+      </main>
+      <footer class="App-footer">
+        
+      </footer>
     </div>
   );
 }
