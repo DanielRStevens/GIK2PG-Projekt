515772239281c2510bf9a5bbac2e5e9bd16e99e8
DanielRStevens <h17daste@du.se>
2023-02-08 15:46:52 +0100

    Fixed up the product list, added to database.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,18 @@
-import './App.css';
-import logo from './images/dalavintage-low-resolution-logo-color-on-transparent-background_2.png'
-import AppProductList from './AppProductList';
+import "./App.css";
+import logo from "./images/dalavintage-low-resolution-logo-color-on-transparent-background_2.png";
+import AppProductList from "./AppProductList";
 
 function App() {
   return (
     <div className="App">
-      <img src={logo} alt="Logo" className="App-logo"/>
-      <div className="App-categories">NEW TEXT</div>
-      <AppProductList/>
+      <img src={logo} alt="Logo" className="App-logo" />
+      <ul className="App-categories">
+        <a href="#new">NEW</a>
+        <a href="#deals">DEALS</a>
+        <a href="#books" className="Activecategory">BOOKS</a>
+        <a href="#newspapers">NEWSPAPERS</a>
+      </ul>
+      <AppProductList />
     </div>
   );
 }
